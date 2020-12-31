0924dc09504667dd0f6c27af7e5192f88fd2cb8f
W.Chao <785808933@qq.com>
2020-12-31 15:20:22 +0800

    添加注释

--- MdSourceFile.java
+++ MdSourceFile.java
@@ -12,11 +12,14 @@ public class MdSourceFile {
     public static char END = '\0';
     protected char curChar = '\n';
     protected char prevChar;
+    // 当前读取字符在当前行的偏移量
     protected int positionInLine = 0;
+    // 正在读取的行数
     protected int numberOfLine = 1;
 
     public MdSourceFile(String fileName) throws MdException {
         try {
+            // 文件流获取bufferedReader -> 增加String(或byte[])转bufferedReader
             reader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName), StandardCharsets.UTF_8));
         } catch (final IOException e) {
             throw error("Error opening input file '%s': %s", fileName, e.getMessage());
@@ -26,6 +29,7 @@ public class MdSourceFile {
     private char read() throws MdException {
         try {
             final int read = reader.read();
+            // 返回结束符或字符
             return read == -1 ? END : (char) read;
         } catch (final IOException e) {
             throw error("Source read error", e.getMessage());
@@ -38,6 +42,7 @@ public class MdSourceFile {
 
     public char readNextChar() throws MdException {
         if (curChar == '\n') {
+            // 换行时更新当前行和字符偏移量
             numberOfLine++;
             positionInLine = 0;
         }
